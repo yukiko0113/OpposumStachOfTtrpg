bc602bd6341de55dadd256f384ace0ba2432d73e
yukiko0113 <shinogeru@gmail.com>
2025-01-09 22:52:26 +0100

    rajout tooltips

--- script.js
+++ script.js
@@ -1,49 +1,84 @@
-import tippy from './node_modules/tippy.js/headless/dist/tippy-headless.esm.js';
+import tippy, { followCursor } from './node_modules/tippy.js/headless/dist/tippy-headless.esm.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const ddBeyond = document.querySelector('#ddBeyond');
+    const ddBeyond = document.querySelector('#ddBeyond');
+    const teste = document.querySelector('#teste');
 
-  function loadTooltipContent(url, selector, callback) {
-    fetch(url)
-      .then(response => response.text())
-      .then(html => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(html, 'text/html');
-        const content = doc.querySelector(selector);
-        if (content) {
-          callback(content.innerHTML);
-        }
-      });
-  }
-
-  loadTooltipContent('tooltip-content.html', '#tooltip-content-ddBeyond', (content) => {
-    tippy(ddBeyond, {
-      content,
-      allowHTML: true,
-      render(instance) {
-        const popper = document.createElement('div');
-        const box = document.createElement('div');
-        
-        box.className = 'tippy-box';
-        box.setAttribute('data-state', 'visible');
-        box.setAttribute('tabindex', '-1');
-        box.innerHTML = instance.props.content;
-        
-        popper.appendChild(box);
-        
-        function onUpdate(prevProps, nextProps) {
-          if (prevProps.content !== nextProps.content) {
-            box.innerHTML = nextProps.content;
-          }
-        }
-
-        return {
-          popper,
-          onUpdate
+    function loadTooltipContent(url, selector, callback) {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                const tempDiv = document.createElement('div');
+                tempDiv.innerHTML = xhr.responseText;
+                const contentElement = tempDiv.querySelector(selector);
+                if (contentElement) {
+                    callback(contentElement.innerHTML);
+                }
+            }
         };
-      },
-      trigger: 'mouseenter focus',
-      touch: ['hold', 500]
+        xhr.send();
+    }
+
+    loadTooltipContent('tooltip-content.html', '#tooltip-content-ddBeyond', (content) => {
+        tippy(ddBeyond, {
+            content,
+            allowHTML: true,
+            followCursor: true,
+            plugins: [followCursor],
+            placement: 'top',
+            offset: [-100, 10],
+            render(instance) {
+                const popper = document.createElement('div');
+                const box = document.createElement('div');
+                
+                box.className = 'tippy-box';
+                box.innerHTML = instance.props.content;
+                
+                popper.appendChild(box);
+                
+                return {
+                    popper,
+                    onUpdate(prevProps, nextProps) {
+                        if (prevProps.content !== nextProps.content) {
+                            box.innerHTML = nextProps.content;
+                        }
+                    }
+                };
+            },
+            trigger: 'mouseenter focus',
+            touch: ['hold', 500]
+        });
     });
-  });
+
+    loadTooltipContent('tooltip-content.html', '#tooltip-content-teste', (content) => {
+        tippy(teste, {
+            content,
+            allowHTML: true,
+            followCursor: true,
+            plugins: [followCursor],
+            placement: 'top',
+            offset: [0, 10],
+            render(instance) {
+                const popper = document.createElement('div');
+                const box = document.createElement('div');
+                
+                box.className = 'tippy-box';
+                box.innerHTML = instance.props.content;
+                
+                popper.appendChild(box);
+                
+                return {
+                    popper,
+                    onUpdate(prevProps, nextProps) {
+                        if (prevProps.content !== nextProps.content) {
+                            box.innerHTML = nextProps.content;
+                        }
+                    }
+                };
+            },
+            trigger: 'mouseenter focus',
+            touch: ['hold', 500]
+        });
+    });
 });
